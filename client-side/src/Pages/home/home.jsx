bec68b25a5c2260a034132d069c67be1f7530158
van <van291630@gmail.com>
2026-04-06 12:49:17 +0300

    apply Navigation Bar

---
+++ client-side/src/Pages/home/home.jsx
@@ -0,0 +1,9 @@
+import NavBar from '../../components/NavBar/NavBar.jsx';
+const Home =()=>{
+    return(
+        <div className="home">
+           <NavBar/>
+        </div>
+    )
+}
+export default Home;
---
+++ client-side/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import "./NavBar.scss";
+import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+import logo from "../../assets/Logo.png";
+const NavBar = ()=>{
+    return(
+        <div className="navbar">
+            <div className="navbarContainer">
+                 <img src={logo} alt="" />
+                 <span className="theMenu">Service</span>
+                 <span className="theMenu">Catagory</span>
+                 <span className="theMenu">About</span>
+                 <span className="SignUpButton">Join <ArrowForwardOutlinedIcon
+                  style={{
+                    padding:"6px",
+                    border:"1px solid #000",
+                    borderRadius:"50%"
+                  }}/></span>
+            </div>
+        </div>
+    )
+}
+export default NavBar;
