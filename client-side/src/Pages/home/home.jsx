5f727eba41d540cb77ca5e931f8eff0fa550539c
van <van291630@gmail.com>
2026-04-08 22:27:15 +0300

    apply Exprole section

--- client-side/src/Pages/home/home.jsx
+++ client-side/src/Pages/home/home.jsx
@@ -5,7 +5,30 @@ import OurStory from '../../components/OurStory/ourStory.jsx';
 import SpotLight from '../../components/spotlight/spotlight.jsx';
 import Zoom from '../../components/immersiveZoom/Zoom.jsx';
 import OurMenu from '../../components/ourMenu/ourMenu.jsx';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import { useEffect } from 'react';
 const Home =()=>{
+     gsap.registerPlugin(ScrollTrigger);
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.fromTo(".menuContainer",
+        {
+           yPercent: 0,
+        }
+        ,{
+        yPercent: -100,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".menuContainer",
+            start: "top bottom",   // when it enters viewport
+            end: "top top",
+            scrub: true,           // smooth scroll-linked animation
+        }
+        });
+
+    }, []);
     return(
         <div className="home">
            <NavBar/>
@@ -14,7 +37,9 @@ const Home =()=>{
            <OurStory/>
            <SpotLight/>
            <Zoom/>
-           <OurMenu/>
+           <div className="menuContainer">
+            <OurMenu/>
+           </div>
         </div>
     )
 }
--- client-side/src/components/ourMenu/ourMenu.jsx
+++ client-side/src/components/ourMenu/ourMenu.jsx
@@ -1,9 +1,64 @@
 import "./ourMenu.scss";
-
+import LeftLine from "../../assets/Left-line.png";
+import rightLine from "../../assets/right-Line.png";
+import coffeeBeanVector from "../../assets/coffeeBeanVector.png";
+import coffeeCup3 from "../../assets/coffeeCup3.png";
+import COFFEEcup2 from "../../assets/COFFEEcup2.png";
+import coffeeCup4 from "../../assets/coffeeCup4.png";
+import COFFEcup1 from "../../assets/COFFEcup1.png";
 const OurMenu = ()=>{
     return(
         <div className="ourMenu">
-            
+            <div className="textContainer">
+              <h1>Our Special Menu</h1>
+              <div className="UnderLineContainer">
+                  <img src={LeftLine} alt="" className="leftLine"/>
+                  <img src={coffeeBeanVector} alt="" className="center"/>
+                  <img src={rightLine} alt="" className="rightLine"/>
+              </div>
+            </div>
+            <div className="itemsContainer">
+                <div className="items">
+                    <div className="imagePart">
+                        <img src={coffeeCup4} alt="" />
+                    </div>
+                    <div className="textPart">
+                        <h1>title</h1>
+                        <p>description</p>
+                        <span>Order</span>
+                    </div>
+                </div>
+                <div className="items">
+                    <div className="imagePart">
+                        <img src={coffeeCup3} alt="" />
+                    </div>
+                    <div className="textPart">
+                        <h1>title</h1>
+                        <p>description</p>
+                        <span>Order</span>
+                    </div>
+                </div>
+                <div className="items">
+                    <div className="imagePart">
+                        <img src={COFFEEcup2} alt="" />
+                    </div>
+                    <div className="textPart">
+                        <h1>title</h1>
+                        <p>description</p>
+                        <span>Order</span>
+                    </div>
+                </div>
+                <div className="items">
+                    <div className="imagePart">
+                        <img src={COFFEcup1} alt="" />
+                    </div>
+                    <div className="textPart">
+                        <h1>title</h1>
+                        <p>description</p>
+                        <span>Order</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
